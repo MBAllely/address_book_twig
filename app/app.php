023db5f66cf61c8e265d71456008f2396ec58a41
Marika Allely <mballely@gmail.com>
2016-02-12 08:43:39 -0800

    add css file

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Contact.php";
+
+    session_start();
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__."/..views"));
+
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('contact_form.html.twig', array('contacts' => Contact::getAll()));
+    });
+
+?>
